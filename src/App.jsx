22ce8257e6370b9fa2b947d6ec226777a2bbd6e2
Konstantin Teterin <teterinkonst@gmail.com>
2025-02-13 16:28:57 +0300

    rewrote from jsx to js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { createElement } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -8,29 +9,64 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  const viteLogoLink = createElement(
+    'a',
+    { href: 'https://vite.dev', target: '_blank' },
+    createElement('img', {
+      src: `${viteLogo}`,
+      className: 'logo',
+      alt: 'Vite logo',
+    }),
+  );
+
+  const reactLogoLink = createElement(
+    'a',
+    { href: 'https://react.dev', target: '_blank' },
+    createElement('img', {
+      src: `${reactLogo}`,
+      className: 'logo react',
+      alt: 'React logo',
+    }),
+  );
+
+  const divLogos = createElement('div', {}, [viteLogoLink, reactLogoLink]);
+
+  const h1 = createElement('h1', {}, 'Vite + React');
+
+  const buttonCounter = createElement(
+    'button',
+    {
+      onClick: () => setCount((count) => count + 1),
+    },
+    `count is ${count}`,
+  );
+
+  const reminderEdit = createElement(
+    'p',
+    {},
+    'Edit <code>src/App.jsx</code> and save to test HMR',
+  );
+
+  const divCard = createElement('div', { className: 'card' }, [
+    buttonCounter,
+    reminderEdit,
+  ]);
+
+  const readTheDocs = createElement(
+    'p',
+    { className: 'read-the-docs' },
+    'Click on the Vite and React logos to learn more',
+  );
+
+  const currentYear = createElement(
+    'div',
+    {},
+    `${new Date().getFullYear()} год`,
+  );
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>{new Date().getFullYear()} год</div>
+      {divLogos} {h1} {divCard} {readTheDocs} {currentYear}
     </>
   );
 }

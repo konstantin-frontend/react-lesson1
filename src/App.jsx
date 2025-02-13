1ffbd761de0c2c8d7c14b1f832ff54a1fd0eadd4
Konstantin Teterin <teterinkonst@gmail.com>
2025-02-13 15:51:49 +0300

    add current year

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+// Декларативный код
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>{new Date().getFullYear()} год</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
